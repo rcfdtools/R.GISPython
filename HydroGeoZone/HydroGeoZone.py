8b34f835951a97408af9355ce7a6521f9a278f9e
r.cfdtools <r.cfdtools@gmail.com>
2021-12-25 13:25:42 -0500

    General update.

--- HydroGeoZone/HydroGeoZone.py
+++ HydroGeoZone/HydroGeoZone.py
@@ -59,8 +59,10 @@ statisticsTableSZHXLS = outputPath+'SubZonaHidrograficaEstadistica.xls'
 fieldAHCode, fieldZHCode, fieldSZHCode= 'COD_AH', 'COD_ZH', 'COD_SZH'
 fieldAHName, fieldZHName, fieldSZHName= 'NOM_AH', 'NOM_ZH', 'NOM_SZH'
 outCoordinateSystem = "PROJCS['MAGNA-SIRGAS / Origen-Nacional',GEOGCS['GCS_MAGNA',DATUM['D_MAGNA',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Transverse_Mercator'],PARAMETER['False_Easting',5000000.0],PARAMETER['False_Northing',2000000.0],PARAMETER['Central_Meridian',-73.0],PARAMETER['Scale_Factor',0.9992],PARAMETER['Latitude_Of_Origin',4.0],UNIT['Meter',1.0]] # GEOGCS['GCS_MAGNA',DATUM['D_MAGNA',SPHEROID['GRS_1980',6378137.0,298.257222101]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]"
+decimalPos = 2
 intersectActive = False # Volver a realizar la intersección espacial y calcular las longitudes de los drenajes intersecados.
 statisticActive = False # Volver a generar estadísticos en DBF y convertir a Excel.
+
 
 # Cabecera
 TitleSeparator('Zonificación hidrográfica de Colombia - Análisis de forma y densidad usando Python')
@@ -216,7 +218,7 @@ print('| ' + fieldAHCode + ' | ' + fieldAHName + ' | Area | Perm | FREQUENCY | S
 print('|---|---|---|---|---|---|---|---|---|')
 cursor = arcpy.SearchCursor(hydroAreaLayer)
 for fila in cursor:
-    print('| ' + str(fila.getValue(fieldAHCode)) + ' | ' + fila.getValue(fieldAHName) + ' | ' + str(fila.getValue('Area')) + ' | ' + str(fila.getValue('Perm')) + ' | ' + str(fila.getValue('FREQUENCY')) + ' | ' + str(fila.getValue('SUM_LDre')) + ' | ' + str(fila.getValue('Kc')) + ' | ' + str(fila.getValue('Dd')) + ' | ' + str(fila.getValue('Dc')) + ' |')
+    print('| ' + str(fila.getValue(fieldAHCode)) + ' | ' + fila.getValue(fieldAHName) + ' | ' + str(round(fila.getValue('Area'),decimalPos)) + ' | ' + str(round(fila.getValue('Perm'),decimalPos)) + ' | ' + str(fila.getValue('FREQUENCY')) + ' | ' + str(round(fila.getValue('SUM_LDre'),decimalPos)) + ' | ' + str(round(fila.getValue('Kc'),decimalPos)) + ' | ' + str(round(fila.getValue('Dd'),decimalPos)) + ' | ' + str(round(fila.getValue('Dc'),decimalPos)) + ' |')
 
 
 print('\nProceso terminado.')
