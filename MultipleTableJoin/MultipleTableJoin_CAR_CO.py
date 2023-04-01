9960c271badd93a9e822307315b3fe2ebcd783e5
r.cfdtools <r.cfdtools@gmail.com>
2023-03-31 19:23:18 -0500

    General update.

--- MultipleTableJoin/MultipleTableJoin_CAR_CO.py
+++ MultipleTableJoin/MultipleTableJoin_CAR_CO.py
@@ -1,21 +1,24 @@
 # -*- coding: UTF-8 -*-
 # Create a dataset file from monthly CAR - Cundinamarca - Colombia from Excel records
 # Data from https://www.car.gov.co/vercontenido/2524
+# Disclaimer: this script version do not create the records marks file and the wind directions file
 
 # Libraries
 import openpyxl
 from openpyxl import Workbook, load_workbook
 import pandas as pd
+import glob
 
 
 # General parameters
 input_path = 'D:/R.GISPython/MultipleTableJoin/CAR_CO/Source/'
 output_path = 'D:/R.GISPython/MultipleTableJoin/CAR_CO/Joined/'
-parameter_value = 'Q_MN_M'  # More info https://github.com/rcfdtools/R.GISPython/tree/main/MultipleTableJoin/CAR_CO
-excel_file = '5bae91ce32989.xlsx'
+parameter_value = 'Q_MEDIA_M'  # More info https://github.com/rcfdtools/R.GISPython/tree/main/MultipleTableJoin/CAR_CO
+excel_file = '5bae92275ec7e.xlsx'
 clean_file = 'Clean_'+excel_file
 pivot_file = 'Pivot_'+parameter_value+'.csv'
 unpivot_file = 'Unpivot_'+parameter_value+'.csv'
+joined_file = 'CAR_Joined.csv'
 head_rows = 15  # Header rows to delete
 index_name = 'Id'
 parameter_name = 'Label'
@@ -25,15 +28,23 @@ month_name = 'Month'
 day_name = 'Day'
 date_name = 'Date'
 value_name = 'Value'
-run_clean = True  # Execute the clean Excel process
-run_pivot_dataset = True  # Execute the pivot dataset creation
-run_unpivot_dataset = True  # Execute the unpivot dataset creation
-year_to_integer = False  # Convert Year values to integer
+run_clean = True  # Execute the clean Excel process. Set False if you are going to join all the files.
+run_pivot_dataset = True  # Execute the pivot dataset creation. Set False if you are going to join all the files.
+run_unpivot_dataset = True  # Execute the unpivot dataset creation. Set False if you are going to join all the files.
+year_to_integer = False  # Convert Year values to integer.
+join_unpivot_files = True  # Set to True if you had all the files processed with unpivot tables and you require one joined file.
 
-book = load_workbook(input_path+excel_file)
-catalog = book['Catalogo']
-book.remove(catalog)
+
+# Load workbook and delete catalog and not station sheets
+book = load_workbook(input_path+excel_file, data_only=True)  # use data_only for read values and not formulas
+delete_sheets = ['Catalogo', 'CATALOGO', 'Resumen', 'RESUMEN', 'Hoja1', 'Hoja2', 'Hoja3', 'Sheet1', 'Sheet2', 'Sheet3']
 sheets = book.sheetnames
+for j in sheets:
+    for i in delete_sheets:
+        if j == i:
+            book.remove(book[i])
+sheets = book.sheetnames
+
 
 # Clean headers and intermediate columns
 if run_clean:
@@ -89,10 +100,21 @@ if run_unpivot_dataset:
     print(df_unpivot, '\n')
     df_unpivot.to_csv(output_path+unpivot_file)
 
-print('\nClean file: %s' % output_path+clean_file,
-      '\nPivot file: %s' % output_path+pivot_file,
-      '\nUnpivot file: %s' % output_path+unpivot_file)
 
+# Join all the unpivot files
+if join_unpivot_files:
+    print('\nJoining the unpivot files\n' + 40 * '-')
+    csv_files = glob.glob(output_path + 'Unpivot_*.csv')
+    print('Joining: %s' %csv_files)
+    df = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
+    df.to_csv(output_path + joined_file, encoding='utf-8', index=False)
+    print(df)
+
+
+print('\nClean file: %s' % output_path + clean_file,
+          '\nPivot file: %s' % output_path + pivot_file,
+          '\nUnpivot file: %s' % output_path + unpivot_file,
+          '\nJoined file: %s' % output_path + joined_file)
 
 # Refs
 # https://blog.aspose.com/cells/insert-and-delete-rows-and-columns-in-excel-using-python/
