d9fe6bcbd8c3ac5d295526fd2fd411bf5327ff33
r.cfdtools <r.cfdtools@gmail.com>
2023-05-06 20:11:06 -0500

    General update.

--- GISMobile/.poi/poi.py
+++ GISMobile/.poi/poi.py
@@ -2,6 +2,7 @@
 
 import os
 import pandas as pd
+import geopandas
 from exif import Image
 
 def decimal_coords(coords, ref):
@@ -32,6 +33,7 @@ def image_coordinates(image_path):
 path = 'D:/R.GISPython/GISMobile/.poi/'
 poi_file = 'poi.csv'
 poi_cols = ['POI', 'Latitude', 'Longitude', 'Altitude', 'Date', 'Name', 'Credit']
+exclude_folder = ['.shp', '.temp']
 directories = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
 
 # Processing directories
@@ -40,19 +42,28 @@ if os.path.isfile(path+poi_file):
 df = pd.DataFrame()
 print('Directories:', directories)
 for i in directories:
-    poi_path = path+i+'/'+poi_file
-    print('Processing: %s' %poi_path)
-    df1 = pd.read_csv(poi_path)
-    df1['POI'] = i
-    df = pd.concat([df, df1], ignore_index=True)
+    if i not in exclude_folder:
+        poi_path = path+i+'/'+poi_file
+        print('Processing: %s' %poi_path)
+        df1 = pd.read_csv(poi_path)  # Esri shapefile does not support datetime fields with parse_dates=['Date']
+        df1['POI'] = i
+        df = pd.concat([df, df1], ignore_index=True)
 df = df[poi_cols]  # Reordering cols
 print(df)
 df.to_csv(path+poi_file, encoding='utf-8', index=False)
 
+gdf = geopandas.GeoDataFrame(df)
+gdf.set_geometry(
+    geopandas.points_from_xy(gdf['Longitude'], gdf['Latitude']),
+    inplace=True, crs='EPSG:4326')
+gdf.drop(['Latitude', 'Longitude'], axis=1, inplace=True)  # optional
+gdf.to_file('.shp/poi.shp')
 
+# Picture properties sample
 img_path = '7/PXL_20230503_190031280.jpg'
 image_coordinates(img_path)
 
 # https://medium.com/spatial-data-science/how-to-extract-gps-coordinates-from-images-in-python-e66e542af354
 # https://stackoverflow.com/questions/141291/how-to-list-only-top-level-directories-in-python
 # https://www.geeksforgeeks.org/python-list-files-in-a-directory/
+# https://gis.stackexchange.com/questions/147156/making-shapefile-from-pandas-dataframe
