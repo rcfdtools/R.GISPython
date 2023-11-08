9d2d7d87c7a1d72cff8f11a711a4cdc41c4ac7a4
r.cfdtools <r.cfdtools@gmail.com>
2023-11-08 17:17:26 -0500

    General update.

--- PMP/Readme.md
+++ PMP/Readme.md
@@ -7,3 +7,5 @@
 
 * https://www.statgraphics.com/probability-distributions
 * https://docs.python.org/es/3/library/math.html
+* https://www.geeksforgeeks.org/python-normal-distribution-in-statistics/
+* https://www.tutorialspoint.com/python_data_science/python_normal_distribution.htm
--- PMP/pmp.py
+++ PMP/pmp.py
@@ -4,6 +4,7 @@ import math
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from scipy.stats import norm
 
 #Prueba de bondad de ajuste usando el método de Kolmogorov-Smirnov usando una función
 def fTestKolmogorov(x, F_Dist):
@@ -22,19 +23,28 @@ def fTestKolmogorov(x, F_Dist):
     else:
         fit, operator = 'doesn’t fit', '<='
     print('* Kolmogorov-Smirnov test (Δo > Δ): %f %s %f (distribution %s the empirical curve)' % (deltao, operator, dFP['dFP'][0], fit))
-    #print('\n%s', dFP)
-    vDeltaKolmogorov['F_Dist'] = delta
+    vDeltaKolmogorovData = [station_name, F_Dist, delta]
+    vDeltaKolmogorov.loc[len(vDeltaKolmogorov)] = vDeltaKolmogorovData
 
 
 # Función de distribución: Gumbel
 # Parameter calculation, Method of Moments
 def fDistGumbel(x):
-    print('\n### Gumbel distribution')
-    alph = math.sqrt(6) * df[station_name].std(ddof=ddof)/math.pi
+    print('\nGumbel distribution')
+    alph = math.sqrt(6) * x[station_name].std(ddof=ddof)/math.pi
     mu = x[station_name].mean() - 0.57721/alph
-    print('\n* α: %f\n* μ: %f' % (alph ,mu))
+    print('* α: %f\n* μ: %f' % (alph ,mu))
     x['F_DGumbel'] = np.exp(-np.exp(-(x[station_name] - mu) / alph))
     fTestKolmogorov(x, 'F_DGumbel')
+
+def fDistNormal(x):
+    print('\nNormal distribution')
+    mu = x[station_name].mean()
+    std = x[station_name].std(ddof=ddof)
+    print('* μ: %f\n* σ: %f' % (mu, std))
+    x['z_score'] = (x[station_name] - mu) / std
+    x['F_DNormal'] = norm.cdf(x['z_score'])
+    fTestKolmogorov(x, 'F_DNormal')
 
 
 #Función de distribución: Weibull (empírica)
@@ -43,12 +53,12 @@ def fDistEmpWeibull(x):
 
 
 # General
-ddof = 1  # Standard deviation normalized
-vDeltaKolmogorov = pd.DataFrame()
 input_path = 'station/'  # Your local input file folder
 station_file = input_path + '25020230.csv'
 station_name = Path(station_file).stem
 print('## Station: %s' %station_name)
+ddof = 1  # Standard deviation normalized
+vDeltaKolmogorov = pd.DataFrame(columns=['Station', 'Dp', 'Delta'])
 df = pd.read_csv(station_file, delimiter=',')
 df = df.dropna()
 df = df.sort_values(by=station_name)
@@ -57,6 +67,9 @@ df['index'] = df.index+1
 print('\nBasic stats\n* n: %d\n* mean: %f\n* std(%d): %f\n* min: %f\n* max: %f' % (df[station_name].count(), df[station_name].mean(), ddof, df[station_name].std(ddof=ddof), df[station_name].min(), df[station_name].max()))
 fDistEmpWeibull(df)
 fDistGumbel(df)
+fDistNormal(df)
+df = df.drop(columns=['z_score'])
 print('\n %s' % df)
+print('\nKolmogorov-Smirnov Δ values\n%s' % vDeltaKolmogorov)
 
 #print(df.to_csv(index=False))
