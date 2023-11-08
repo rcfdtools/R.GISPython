a820dbc95534f0baac25a6783b0471974bf5a5c6
r.cfdtools <r.cfdtools@gmail.com>
2023-11-08 11:18:24 -0500

    General update.

--- PMP/pmp.py
+++ PMP/pmp.py
@@ -5,62 +5,58 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
+#Prueba de bondad de ajuste usando el método de Kolmogorov-Smirnov usando una función
+def fTestKolmogorov(x, F_Dist):
+    dFP = pd.DataFrame()
+    dFP['dFP'] = abs(x['P_E']-x[F_Dist])
+    dFP = dFP.sort_values(by='dFP', ascending=[False])
+    dFP = dFP.reset_index(drop=True)
+    n = len(dFP)
+    if (n < 35):
+        deltao = 0.000003848186*n**4-0.00033109622*n**3+0.010220554*n**2-0.141035449935*n+1.07518805168
+    else:
+        deltao = 1.36/math.sqrt(n)
+    delta = dFP['dFP'][0]
+    if (deltao > delta):
+        fit, operator = 'fit', '>'
+    else:
+        fit, operator = 'doesn’t fit', '<='
+    print('* Kolmogorov-Smirnov test (Δo > Δ): %f %s %f (distribution %s the empirical curve)' % (deltao, operator, dFP['dFP'][0], fit))
+    #print('\n%s', dFP)
+    vDeltaKolmogorov['F_Dist'] = delta
 
-#Función de distribución: Gumbel
+
+# Función de distribución: Gumbel
+# Parameter calculation, Method of Moments
 def fDistGumbel(x):
-    print('\n### Distribución de Gumbel (Station: %s)' %station_name)
-    #Parameter calculation, Method o f Moments
-    vDistID = 2 # Distribution ID
-    alph = math.sqrt(6) * np.std(x[station_name])/math.pi
+    print('\n### Gumbel distribution')
+    alph = math.sqrt(6) * df[station_name].std(ddof=ddof)/math.pi
     mu = x[station_name].mean() - 0.57721/alph
-    print('\nn: %f\nmean: %f\nalpha: %f\nmu: %f' % (x[station_name].count(), x[station_name].mean(), alph ,mu))
-    #F_DGumbel = c()
-    #for (i in 1:n) {F_DGumbel[i] <- exp(-exp(-(x[i]-mu)/alph))}
-    #for i in x:
-    #F_DGumbel[i] = math.exp(-math.exp(-(x[i] - mu) / alph))
-    #F_DGumbel = F_DGumbel #Set as global var
+    print('\n* α: %f\n* μ: %f' % (alph ,mu))
     x['F_DGumbel'] = np.exp(-np.exp(-(x[station_name] - mu) / alph))
-    print('\n %s' %df)
-    #print(data.frame(x, P_E, F_DGumbel))
-    vGraphTitle = ("GUMBEL DISTRIBUTION (Id: ",vStationName,")")
-    #plot (x, F_DGumbel, type="l", pch=20, col="red", lty=2, xlab="", ylab="Cumulative probability", cex.main=vGrphTitSize, main=vGraphTitle)
-    #lines (x, P_E, pch=18, col="blue", type="b", lty=1)
-    #grid()
-    #legend ("bottomright", legend=c("Gumbel Distribution", "Empirical Distribution"),
-    #        lty=1:2, col=c("red", "blue"), cex=0.8 , text.font=3, title= "r.cfdtools@gmail.com")
-    #D.GUMBEL>> Cálculo de probabilidades y quantiles - Precipitación para diferentes periodos de retorno
-    #XTrDGumbel = mu-log((-log(p, exp(1))), exp(1)) * alph
-    #print("\n")
-    #print(data.frame(Tr, pt, XTrDGumbel)) #Tabla de resultados
-    #vMaxXTr[2] = max(XTrDGumbel)
-    #vMinXTr[2] = min(XTrDGumbel)
-    #vGraphTitle = ("XTrDGumbel vs Return period (Id: ",vStationName,")")
-    #plot(Tr, XTrDGumbel, pch=20, col="#000099", bg="#FF6666", type="b" , lty=1, ylab = "XTrDGumbel", xlab = "Return period (years)", cex.main=vGrphTitSize, main = vGraphTitle)
-    #grid()
-    #fTestKolmogorov(vDistID,P_E,F_DGumbel)
+    fTestKolmogorov(x, 'F_DGumbel')
 
-#Función de distribución: empírica propuesta por Weibull
+
+#Función de distribución: Weibull (empírica)
 def fDistEmpWeibull(x):
-  '''P_E = c()
-  for (i in 1:n) {P_E[i] <- i / (n+1)};
-  cat("\n")
-  print(data.frame(x, P_E))                     #Mostrar los valores ordenados de x y la distribucion empirica de Weibull
-  P_E <<- P_E                                   #Definir P_E como global
-  '''
-  x['P_E'] = x[station_name] / (x.len()+1)
-
+    x['P_E'] = x['index'] / (len(x[station_name])+1)
 
 
 # General
+ddof = 1  # Standard deviation normalized
+vDeltaKolmogorov = pd.DataFrame()
 input_path = 'station/'  # Your local input file folder
 station_file = input_path + '25020230.csv'
 station_name = Path(station_file).stem
-print('Station: %s' %station_name)
+print('## Station: %s' %station_name)
 df = pd.read_csv(station_file, delimiter=',')
 df = df.dropna()
 df = df.sort_values(by=station_name)
-print(df)
-#print(df['year'])
-df['F_DGumbel'] = np.nan
+df = df.reset_index(drop=True)
+df['index'] = df.index+1
+print('\nBasic stats\n* n: %d\n* mean: %f\n* std(%d): %f\n* min: %f\n* max: %f' % (df[station_name].count(), df[station_name].mean(), ddof, df[station_name].std(ddof=ddof), df[station_name].min(), df[station_name].max()))
 fDistEmpWeibull(df)
-fDistGumbel(df,station_name)
+fDistGumbel(df)
+print('\n %s' % df)
+
+#print(df.to_csv(index=False))
